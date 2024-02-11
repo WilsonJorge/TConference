0fd26560d929f363ddb41226683c5cf02d757b18
Wilson Macuacua <macuacuaw330@gmail.com>
2024-02-11 21:41:45 +0200

    Begining my Front-end

---
+++ src/Components/Profile.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Profile (){
+    return(
+
+        <div>
+            <h1>Profile</h1>
+            <p>Aqui voce pode ver o seu perfil editar</p>
+        </div>
+    );
+
+}
+export default Profile
---
+++ src/Components/Settings.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Settings(){
+    return(
+        <div>
+            <h1>Settings</h1>
+            <p>Aqui voce fara as configuracoes dos Perfils</p>
+        </div>
+    )
+}
+export default Settings;
